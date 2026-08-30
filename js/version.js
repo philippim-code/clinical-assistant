5476804d56e4b846dbe296c57a2e14c5de8c2685
philippim-code <philippim@outlook.com>
2026-08-29 22:33:25 -0400

    Bump appearance preview to dev24

--- js/version.js
+++ js/version.js
@@ -2,7 +2,7 @@
 (function(){
   'use strict';
 
-  const VERSION='1.9.0-dev23';
+  const VERSION='1.9.0-dev24';
 
   const APPEARANCE_STYLESHEETS=[
     'css/appearance-overhaul.css?v='+encodeURIComponent(VERSION),
@@ -21,7 +21,8 @@
     'css/appearance-overhaul-dev20.css?v='+encodeURIComponent(VERSION),
     'css/appearance-overhaul-dev21.css?v='+encodeURIComponent(VERSION),
     'css/appearance-overhaul-dev22.css?v='+encodeURIComponent(VERSION),
-    'css/appearance-overhaul-dev23.css?v='+encodeURIComponent(VERSION)
+    'css/appearance-overhaul-dev23.css?v='+encodeURIComponent(VERSION),
+    'css/appearance-overhaul-dev24.css?v='+encodeURIComponent(VERSION)
   ];
 
   const APPEARANCE_SCRIPTS=[
@@ -67,7 +68,7 @@
       'appearanceOverhaulDev5Styles','appearanceOverhaulDev6Styles','appearanceOverhaulDev7Styles','appearanceOverhaulDev8Styles',
       'appearanceOverhaulDev9Styles','appearanceOverhaulDev10Styles','appearanceOverhaulDev11Styles','appearanceOverhaulDev12Styles',
       'appearanceOverhaulDev13Styles','appearanceOverhaulDev20Styles','appearanceOverhaulDev21Styles','appearanceOverhaulDev22Styles',
-      'appearanceOverhaulDev23Styles'
+      'appearanceOverhaulDev23Styles','appearanceOverhaulDev24Styles'
     ];
     const id=ids[index]||('appearanceOverhaulStyles'+index);
     if(document.getElementById(id))return;
